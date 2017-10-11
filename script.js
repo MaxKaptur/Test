d3ccb7f824c056767dfa4ac348e45df66d712eba
Max Kaptur <kapturmax@mail.ru>
2017-10-11 23:50:26 +0300

    DZ

---
+++ script.js
@@ -0,0 +1,8 @@
+var x=prompt('Vvedite uravnenie')
+var i=x.length;
+
+alert('Uravnenie '+x+ ' soderzit '+x.length+ ' simvolov'.match(/g\(\)/) ); // "g()")
+alert( 'x.match(/\w\.\w/) ');
+console.log('Kol-vo simbolov '+x.length);
+
+
